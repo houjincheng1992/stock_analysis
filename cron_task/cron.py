5a034b201d3420747eadb6689a85bb546e8610e0
houjincheng <houjincheng@baidu.com>
2017-10-16 15:41:47 +0800

    add ranklist parse

--- cron_task/cron.py
+++ cron_task/cron.py
@@ -1,4 +1,5 @@
 from rank_list.utils import get_last_charge_detail_by_inst
+from rank_list.utils import get_last_ranklist_detail
 import logging
 
 logger = logging.getLogger("django.%s" % (__name__))
@@ -6,4 +7,9 @@ logger = logging.getLogger("django.%s" % (__name__))
 
 def get_last_charge_detail_by_inst_scheduled_job():
     get_last_charge_detail_by_inst()
+    logger.info("done")
+
+
+def get_last_ranklist_detail_scheduled_job():
+    get_last_ranklist_detail()
     logger.info("done")

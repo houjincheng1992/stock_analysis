c8e3e594f60971c24a1aaccca37411f9cf3436d6
houjincheng <houjincheng@baidu.com>
2017-10-19 19:11:03 +0800

    cron modify

--- cron_task/cron.py
+++ cron_task/cron.py
@@ -1,6 +1,7 @@
 from rank_list.utils import get_last_charge_detail_by_inst
 from rank_list.utils import get_last_ranklist_detail
 from stock_classified.utils import get_last_stock_basic_info
+from stock_classified.utils import get_realtime_stock_charge_info
 import logging
 
 logger = logging.getLogger("django.%s" % (__name__))
@@ -30,4 +31,14 @@ def get_last_stock_basic_info_scheduled_job():
     :return:
     """
     get_last_stock_basic_info()
+    logger.info("done")
+
+
+def get_realtime_stock_charge_info_scheduled_job():
+    """
+    real time stock charge info parse
+    :return: 
+    """
+    code_list = ["000786", "300654", "000878", "603367", "000968", "300240", "000605", "300701"]
+    get_realtime_stock_charge_info()
     logger.info("done")
--- stock_analysis/settings.py
+++ stock_analysis/settings.py
@@ -135,9 +135,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 CRONJOBS = [
-    ('*/1 * * * *', 'cron_task.cron.get_last_charge_detail_by_inst_scheduled_job'),
-    ('*/1 * * * *', 'cron_task.cron.get_last_ranklist_detail_scheduled_job'),
-    ('*/1 * * * *', 'cron_task.cron.get_last_stock_basic_info_scheduled_job'),
+    ('* */1 * * *', 'cron_task.cron.get_last_charge_detail_by_inst_scheduled_job'),
+    ('* */1 * * *', 'cron_task.cron.get_last_ranklist_detail_scheduled_job'),
+    ('* */1 * * *', 'cron_task.cron.get_last_stock_basic_info_scheduled_job'),
+    ('*/1 * * * *', 'cron_task.cron.get_realtime_stock_charge_info_scheduled_job'),
 ]
 
 # A sample logging configuration. The only tangible logging
